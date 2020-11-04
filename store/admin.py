002b1ec1274b89f3a0e36a1438d19e005bda6206
sukhrobbek <mr.suhrobbek@gmail.com>
2020-11-04 01:18:25 -0500

    First commit

---
+++ store/admin.py
@@ -0,0 +1,33 @@
+from django.contrib import admin
+
+# Register your models here.
+from store.models import Customer, Product, Order, OrderItem, ShippingAddress
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'category')
+    list_filter = ('category',)
+
+
+admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(ShippingAddress)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
